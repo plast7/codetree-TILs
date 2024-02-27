c4fbe5b5076f12b095e527ac2b13b951048d6af8
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-02-27 18:02:24 +0900

    Title: 원소 10개의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240227/원소 10개의 합/sum-of-10-elements.py
+++ 240227/원소 10개의 합/sum-of-10-elements.py
@@ -1,11 +1,11 @@
 # 배열을 구현하여 주어진 수를 입력받습니다.
 arr = list(map(int, input().split()))
+n = 10
 sum_val = 0
 
 # 입력받은 수들을  sum에 더합니다.
-for elem in arr:
-	num = elem
-	sum_val += num
+for i in range(n):
+	sum_val += ar[i]
 	
 # sum을 출력합니다.
 print(sum_val)
