dac9b941d91ae35610f0b6143a7f5617800b7f76
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-02-27 19:19:24 +0900

    Title: 원소 10개의 합, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240227/원소 10개의 합/sum-of-10-elements.py
+++ 240227/원소 10개의 합/sum-of-10-elements.py
@@ -1,15 +1,11 @@
 # 배열을 구현하여 주어진 수를 입력받습니다.
-arr = list(map(int, input().split()))
+arr = list(map(float, input().split()))
 n = 10
 sum_val = 0
 
 # 입력받은 수들을  sum에 더합니다.
 for i in range(n):
 	sum_val += arr[i]
-
-def f(x):
-    sum_val += x
-
-f(0)
+	
 # sum을 출력합니다.
 print(sum_val)
