42184c7e0a352f284dd1a51bff52dc5636978f6e
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-02-28 13:57:02 +0900

    Title: 원소 10개의 합, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240228/원소 10개의 합/sum-of-10-elements.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(100000)
+
+# 배열을 구현하여 주어진 수를 입력받습니다.
+arr = list(map(int, input().split()))
+sum_val = 0
+
+# 입력받은 수들을  sum에 더합니다.
+for elem in arr:
+    num = elem
+    sum_val += num
+
+# sum을 출력합니다.
+print(sum_val)
