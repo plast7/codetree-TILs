9b9c64444f4fe471bd964b2a6de19e019007ff4f
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-03-18 14:51:19 +0900

    Title: 100을 넘기는 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240318/100을 넘기는 순간/as-soon-as-hit-100.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[55];
+
+int ss;
+
+int main() {
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+
+    cout << fixed;
+    cout.precision(1);
+
+    for(int i = 1; i <= n; i++) {
+        ss += a[i];
+        if(a[i] >= 100) {
+            cout << ss << endl;
+            cout << (double)ss / i << endl;
+            return 0;
+        }
+    }
+}
