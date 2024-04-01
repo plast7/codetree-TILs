4363c65af1d89529e61531f44747256da3a6b471
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-04-01 17:35:04 +0900

    Title: 문자열 계속 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240401/문자열 계속 지우기/keep-removing-string.py
+++ 240401/문자열 계속 지우기/keep-removing-string.py
@@ -4,7 +4,7 @@ arr=list(a)
 
 while True:
     idx=''.join(arr).index(b) # b 가 a 에 포함된 최초의 위치 
-    arr=arr[:idx]+arr[idx:] # slicing 
+    arr=arr[:idx]+arr[idx+len(b):] # slicing 
     if b not in ''.join(arr): #있을 떄까지 반복 
         print(''.join(arr))
         break
