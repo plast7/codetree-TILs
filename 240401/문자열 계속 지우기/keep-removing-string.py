62a50f6a8a86694f3e2d4623584ae38285829d78
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-04-01 17:36:45 +0900

    Title: 문자열 계속 지우기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240401/문자열 계속 지우기/keep-removing-string.py
+++ 240401/문자열 계속 지우기/keep-removing-string.py
@@ -3,6 +3,9 @@ b=input()
 arr=list(a)
 
 while True:
+    if b not in ''.join(arr): #있을 떄까지 반복 
+        print(''.join(arr))
+        break
     idx=''.join(arr).index(b) # b 가 a 에 포함된 최초의 위치 
     arr=arr[:idx]+arr[idx+len(b):] # slicing 
     if b not in ''.join(arr): #있을 떄까지 반복 
