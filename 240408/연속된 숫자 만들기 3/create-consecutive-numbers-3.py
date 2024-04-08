db3b5582020b6ab7c9c8e3d482bf32b19ddd52bf
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-04-08 20:59:54 +0900

    Title: 연속된 숫자 만들기 3, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240408/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240408/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,18 +1,29 @@
-a, b, c = list(map(int, input().split()))
+import sys
 
-f_dist = abs(a-b)
-s_dist = abs(b-c)
+# 변수 선언 및 입력
+a = list(map(int, input().split()))
 
-# 1, 1 이 되면 끝
-# 둘 다 2 이하여도 한 번만 하고 끝
-# 두 조건 다 아니면, 둘 중 사이가 먼 거리 값에서 1 뺀 값이 정답
-count = 0
+a.sort()
 
-if f_dist == 1 and s_dist == 1:
-    count = 0
-elif f_dist <= 2 and s_dist <= 2:
-    count = 1
-else:
-    count = max(f_dist, s_dist) - 1
+# Case 1. 3개의 숫자가 전부 연속한 경우
+# 이 경우에는 이동할 필요가 없으므로
+# 최대 이동 횟수는 0이 됩니다.
+if a[0] + 1 == a[1] and a[1] + 1 == a[2]:
+    print(0)
+    sys.exit()
 
-print(count)
+max_move = 0
+# 그렇지 않은 경우 왼쪽 끝 사람이 이동하는 경우와
+# 오른쪽 끝 사람이 이동하는 경우로 나뉘게 됩니다.
+# 각각 거리를 한칸씩 좁혀가면서 이동하는 방식이 최대 이동횟수를 만들기 때문에
+# Case 2. 왼쪽 끝 사람이 가장 먼저 이동하는 경우
+# 최대 이동 횟수는 (오른쪽 끝 사람과 가운데 사람의 거리) - 1 회가 됩니다.
+move = a[2] - a[1] - 1
+max_move = max(max_move, move)
+
+# Case 3. 오른쪽 끝 사람이 가장 먼저 이동하는 경우
+# 최대 이동 횟수는 (왼쪽 끝 사람과 가운데 사람의 거리) - 1 회가 됩니다.
+move = a[1] - a[0] - 1
+max_move = max(max_move, move)
+
+print(max_move)
