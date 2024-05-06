2e2ee47482809d98b3aeeabdcd53257c64e885ea
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-05-06 20:31:53 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 1353ms, Memory: 23MB, Status: Passed - CodeTree

--- 240506/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240506/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,43 +1,38 @@
-import java.io.*;
-import java.util.stream.Stream;
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-
-    static int n, k;
-    static List<Integer> list = new ArrayList<>();
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        k = sc.nextInt();
-
-        recursive(0);
-
+    public static int k, n;
+    public static ArrayList<Integer> selectedNums = new ArrayList<>();
+    
+    // 선택된 원소들을 출력해줍니다.
+    public static void printPermutation() {
+        for(int i = 0; i < selectedNums.size(); i++)
+            System.out.print(selectedNums.get(i) + " ");
+        System.out.println();
     }
 
-    static void recursive(int index) {
-
-        if (index == k) {
-            for (int i = 0; i < k; i++) {
-                System.out.print(list.get(i) + " ");
-            }
-            System.out.println();
+    public static void findPermutations(int cnt) {
+        // n개를 모두 뽑은 경우 답을 출력해줍니다.
+        if(cnt == n) {
+            printPermutation();
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-
-            list.add(i);
-            recursive(index + 1);
-            list.remove(list.size() - 1);
-
+        // 1부터 k까지의 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
+        for(int i = 1; i <= k; i++) {
+            selectedNums.add(i);
+            findPermutations(cnt + 1);
+            selectedNums.remove(selectedNums.size() - 1);
         }
-
-
-
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        k = sc.nextInt();
+        n = sc.nextInt();
+        
+		findPermutations(0);
+	}
 }
