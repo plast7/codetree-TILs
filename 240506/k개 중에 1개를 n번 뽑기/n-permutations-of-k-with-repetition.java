be0b7ea31fdaf5e8b4ebd1d56c9c2253c8dd94d5
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-05-06 20:30:08 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240506/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.stream.Stream;
+import java.util.*;
+
+public class Main {
+
+    static int n, k;
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        recursive(0);
+
+    }
+
+    static void recursive(int index) {
+
+        if (index == k) {
+            for (int i = 0; i < k; i++) {
+                System.out.print(list.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            list.add(i);
+            recursive(index + 1);
+            list.remove(list.size() - 1);
+
+        }
+
+
+
+    }
+
+
+}```
