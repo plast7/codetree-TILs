5643650c28c34191f6e1b53128bd18372c126ccb
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-05-13 19:34:02 +0900

    Title: 양옆 문자 제거하기, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240513/양옆 문자 제거하기/del-both-side-char.js
+++ 240513/양옆 문자 제거하기/del-both-side-char.js
@@ -1,13 +1,3 @@
-const fs=require("fs");
-let input=fs.readFileSync(0).toString();
-let size=input.length;
-input = input.slice(0,1)+input.slice(2,)
-let answer=input.slice(0,size-3)+input.slice(size-2,);
-// console.log(input.slice(0,1));
-// console.log(input.slice(2,size-2));
-// console.log(input.slice(size-1));
-console.log(answer);
-// 변수 선언 및 입력
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
