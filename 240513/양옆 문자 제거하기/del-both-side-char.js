01c496d9965ceae360870baafe646d0eed56a595
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-05-13 19:33:37 +0900

    Title: 양옆 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240513/양옆 문자 제거하기/del-both-side-char.js
+++ 240513/양옆 문자 제거하기/del-both-side-char.js
@@ -7,3 +7,19 @@ let answer=input.slice(0,size-3)+input.slice(size-2,);
 // console.log(input.slice(2,size-2));
 // console.log(input.slice(size-1));
 console.log(answer);
+// 변수 선언 및 입력
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+// 문자열의 길이를 구합니다.
+let leng = input.length;
+
+// 앞에서 2번째 원소를 제거합니다. (이때 문자열의 길이가 1 감소하는것을 반드시 기억합니다)
+input = input.slice(0, 1) + input.slice(2);
+leng -= 1;
+
+// 뒤에서 2번째 원소를 제거합니다.
+input = input.slice(0, leng - 2) + input.slice(leng - 1);
+
+// 앞에서 2번째, 뒤에서 2번째 원소가 제거된 문자열을 출력합니다.
+console.log(input);
