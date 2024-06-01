6174cfdc6c316b0cc47a002835eee6723033bd67
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 16:16:42 +0900

    Title: 특정 구역 내의 공, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240601/특정 구역 내의 공/balls-within-a-particular-zone.java
+++ 240601/특정 구역 내의 공/balls-within-a-particular-zone.java
@@ -1,29 +1,32 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-    static final int inf = 1 << 30;
-    static int[] x = new int[3010], y = new int[3010];
-    static int[] t = new int[3010];
-    static int xz, yz;
-    static char[][] arr = new char[6010][6010];
-    static int gz;
-    static int[] mx = {-1, 0, 1, 0};
-    static int[] my = {0, -1, 0, 1};
-    static Stack<Short> sx = new Stack<>(), sy = new Stack<>();
+
+    public static final int INF = 1 << 30;
     
-    static int compress(int[] a, int z) {
-        System.arraycopy(a, 0, t, 0, t.length);
+    public static int[] x = new int[3010];
+    public static int[] y = new int[3010];
+    public static int[] t = new int[3010];
+    
+    public static int compress(int[] a, int z) {
+        System.arraycopy(a, 0, t, 0, z);
         Arrays.sort(t, 0, z);
-        int nz = (int) Arrays.stream(Arrays.copyOf(t, z)).distinct().count();
-        for (int i = 0; i < z; ++i) {
+        int nz = (int) Arrays.stream(t, 0, z).distinct().count();
+        for (int i = 0; i < z; i++) {
             a[i] = Arrays.binarySearch(t, 0, nz, a[i]) * 2;
         }
         return nz * 2;
     }
-
-    static void put(int x, int y) {
+    
+    public static int xz, yz;
+    public static char[][] arr = new char[6010][6010];
+    public static int gz;
+    public static int[] mx = {-1, 0, 1, 0};
+    public static int[] my = {0, -1, 0, 1};
+    public static Stack<Short> sx = new Stack<>();
+    public static Stack<Short> sy = new Stack<>();
+    
+    public static void put(int x, int y) {
         if (x < 0 || x >= xz || y < 0 || y >= yz) return;
         if (arr[x][y] == 'X') return;
         gz += (arr[x][y] == 'C') ? 1 : 0;
@@ -31,35 +34,37 @@ public class Main {
         sx.push((short) x);
         sy.push((short) y);
     }
-
-    static void dfs(int startx, int starty) {
+    
+    public static void dfs(int startx, int starty) {
         put(startx, starty);
         while (!sx.isEmpty()) {
             int x = sx.pop();
             int y = sy.pop();
-            for (int i = 0; i < 4; ++i) {
+            for (int i = 0; i < 4; i++) {
                 int nx = x + mx[i];
                 int ny = y + my[i];
                 put(nx, ny);
             }
         }
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
         int m = sc.nextInt();
         int z = 0;
-        x[z] = y[z] = -inf; z++;
-        x[z] = y[z] = inf; z++;
         
-        for (int i = 0; i < 2 * n; ++i) {
+        x[z] = y[z] = -INF; z++;
+        x[z] = y[z] = INF; z++;
+        
+        for (int i = 0; i < 2 * n; i++) {
             x[z] = sc.nextInt();
             y[z] = sc.nextInt();
             z++;
         }
-        for (int i = 0; i < m; ++i) {
+        
+        for (int i = 0; i < m; i++) {
             x[z] = sc.nextInt();
             y[z] = sc.nextInt();
             z++;
@@ -67,28 +72,45 @@ public class Main {
         
         xz = compress(x, z);
         yz = compress(y, z);
-        for (char[] row : arr) Arrays.fill(row, '.');
         
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < 6010; i++) {
+            Arrays.fill(arr[i], '.');
+        }
+        
+        for (int i = 0; i < n; i++) {
             int a = i * 2 + 2;
             int b = i * 2 + 3;
             int x1 = x[a], y1 = y[a], x2 = x[b], y2 = y[b];
             if (x1 == x2) {
-                if (y1 > y2) { int temp = y1; y1 = y2; y2 = temp; }
-                while (y1 <= y2) { arr[x1][y1++] = 'X'; }
+                if (y1 > y2) {
+                    int temp = y1;
+                    y1 = y2;
+                    y2 = temp;
+                }
+                while (y1 <= y2) {
+                    arr[x1][y1] = 'X';
+                    y1++;
+                }
             } else {
-                if (x1 > x2) { int temp = x1; x1 = x2; x2 = temp; }
-                while (x1 <= x2) { arr[x1++][y1] = 'X'; }
+                if (x1 > x2) {
+                    int temp = x1;
+                    x1 = x2;
+                    x2 = temp;
+                }
+                while (x1 <= x2) {
+                    arr[x1][y1] = 'X';
+                    x1++;
+                }
             }
         }
         
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; i++) {
             int a = i + 2 * n + 2;
             arr[x[a]][y[a]] = 'C';
         }
         
         int ans = 0;
-        for (int i = 0; i < m; ++i) {
+        for (int i = 0; i < m; i++) {
             int a = i + 2 * n + 2;
             gz = 0;
             dfs(x[a], y[a]);
