156fd3cb551754585870bc42685e39634b2b670f
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 15:35:26 +0900

    Title: 모든 점 방문하기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240601/모든 점 방문하기/visit-all-dots.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static final int MAXN = 1005;
+
+    public static int N;
+    public static int[] cows = new int[MAXN];
+    public static int[][] best = new int[MAXN][2], best2 = new int[MAXN][2];
+
+    public static void mini(int[] a, int idx, int b) {
+        if (b < a[idx]) a[idx] = b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Arrays.fill(best[0], Integer.MAX_VALUE);
+
+        N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            cows[i] = sc.nextInt();
+        }
+
+        cows[++N] = 0;
+
+        Arrays.sort(cows, 1, N + 1);
+
+        for (int i = 1; i <= N; i++) {
+            if (cows[i] == 0) {
+                best[i][0] = 0;
+            }
+        }
+
+        for (int len = 1; len < N; len++) {
+            int ccount = N - len;
+
+            for (int[] row : best2) Arrays.fill(row, Integer.MAX_VALUE);
+
+            for (int i = 1; i + len <= N + 1; i++) {
+                mini(best2[i - 1], 0, best[i][0] + ccount * (cows[i] - cows[i - 1]));
+                mini(best2[i - 1], 0, best[i][1] + ccount * (cows[i + len - 1] - cows[i - 1]));
+
+                mini(best2[i], 1, best[i][0] + ccount * (cows[i + len] - cows[i]));
+                mini(best2[i], 1, best[i][1] + ccount * (cows[i + len] - cows[i + len - 1]));
+            }
+
+            for (int i = 0; i < best.length; i++) {
+                best[i][0] = best2[i][0];
+                best[i][1] = best2[i][1];
+            }
+        }
+
+        if (best[1][1] < best[1][0]) {
+            best[1][0] = best[1][1];
+        }
+
+        System.out.println(best[1][0]);
+    }
+}
