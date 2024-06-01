d4749f7931550e6767593555b7decf94583325d1
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 20:39:16 +0900

    Title: 특정 구역 내의 공, Time: 909ms, Memory: 134MB, Status: Passed - CodeTree

--- 240601/특정 구역 내의 공/balls-within-a-particular-zone.java
+++ 240601/특정 구역 내의 공/balls-within-a-particular-zone.java
@@ -1,46 +1,43 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-
-    public static final int inf = 1 << 30;
-
+    public static final int INF = 1 << 30;
     public static int[] x = new int[3010];
     public static int[] y = new int[3010];
     public static int[] t = new int[3010];
+    public static int xz, yz;
+    public static char[][] arr = new char[6010][6010];
+    public static int gz;
+    public static int[] mx = {-1, 0, 1, 0};
+    public static int[] my = {0, -1, 0, 1};
+    public static Stack<Short> sx = new Stack<>();
+    public static Stack<Short> sy = new Stack<>();
 
     public static int compress(int[] a, int z) {
         System.arraycopy(a, 0, t, 0, z);
         Arrays.sort(t, 0, z);
-        int nz = (int) Arrays.stream(t, 0, z).distinct().count();
+        int nz = 1;
+        for (int i = 1; i < z; i++) {
+            if (t[i] != t[i - 1]) {
+                t[nz++] = t[i];
+            }
+        }
         for (int i = 0; i < z; i++) {
             a[i] = Arrays.binarySearch(t, 0, nz, a[i]) * 2;
         }
         return nz * 2;
     }
 
-    public static int xz, yz;
-    public static char[][] arr = new char[6010][6010];
-
-    public static int gz;
-    public static int[] mx = {-1, 0, 1, 0};
-    public static int[] my = {0, -1, 0, 1};
-
-    public static Stack<Short> sx = new Stack<>();
-    public static Stack<Short> sy = new Stack<>();
-
     public static void put(int x, int y) {
         if (x < 0 || x >= xz || y < 0 || y >= yz) return;
         if (arr[x][y] == 'X') return;
-        gz += (arr[x][y] == 'C' ? 1 : 0);
+        gz += (arr[x][y] == 'C') ? 1 : 0;
         arr[x][y] = 'X';
         sx.push((short) x);
         sy.push((short) y);
     }
 
     public static void dfs(int startx, int starty) {
-        // sx, sy should be empty
         put(startx, starty);
         while (!sx.isEmpty()) {
             int x = sx.pop();
@@ -58,10 +55,9 @@ public class Main {
 
         int n = sc.nextInt();
         int m = sc.nextInt();
-
         int z = 0;
-        x[z] = y[z] = -inf; z++;
-        x[z] = y[z] = inf; z++;
+        x[z] = y[z] = -INF; z++;
+        x[z] = y[z] = INF; z++;
         for (int i = 0; i < 2 * n; i++) {
             x[z] = sc.nextInt();
             y[z] = sc.nextInt();
@@ -72,14 +68,11 @@ public class Main {
             y[z] = sc.nextInt();
             z++;
         }
-
         xz = compress(x, z);
         yz = compress(y, z);
-
-        for (int i = 0; i < arr.length; i++) {
-            Arrays.fill(arr[i], '.');
+        for (char[] row : arr) {
+            Arrays.fill(row, '.');
         }
-
         for (int i = 0; i < n; i++) {
             int a = i * 2 + 2;
             int b = i * 2 + 3;
@@ -106,12 +99,10 @@ public class Main {
                 }
             }
         }
-
         for (int i = 0; i < m; i++) {
             int a = i + 2 * n + 2;
             arr[x[a]][y[a]] = 'C';
         }
-
         int ans = 0;
         for (int i = 0; i < m; i++) {
             int a = i + 2 * n + 2;
@@ -119,7 +110,6 @@ public class Main {
             dfs(x[a], y[a]);
             ans = Math.max(ans, gz);
         }
-
         System.out.println(ans);
     }
 }
