9c0c6405b02d199bb0228e8165402146c93e9505
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 18:01:06 +0900

    Title: 겹쳐있는 정사각형의 총 면적, Time: 950ms, Memory: 48MB, Status: Passed - CodeTree

--- 240601/겹쳐있는 정사각형의 총 면적/the-total-area-of-the-overlapping-square.java
+++ 240601/겹쳐있는 정사각형의 총 면적/the-total-area-of-the-overlapping-square.java
@@ -17,22 +17,23 @@ public class Main {
 
         TreeSet<Pair> st = new TreeSet<>();
         List<Pair> res = new ArrayList<>();
-        Iterator<Pair> ita, itb;
+        
         for (int i = 0, j = 0; i < S.size() && res.size() < 2; i++) {
             while (S.get(j).x + K <= S.get(i).x) {
                 st.remove(new Pair(S.get(j).y, j));
                 j++;
             }
 
-            st.add(new Pair(S.get(i).y, i));
-            ita = st.lower(new Pair(S.get(i).y, i));
-            itb = st.higher(new Pair(S.get(i).y, i));
-
-            if (ita != null && S.get(i).y < ita.get().x + K) {
-                res.add(new Pair(i, ita.get().y));
+            Pair inserted = new Pair(S.get(i).y, i);
+            st.add(inserted);
+            Pair ita = st.lower(inserted);
+            Pair itb = st.higher(inserted);
+            
+            if (ita != null && S.get(i).y < ita.x + K) {
+                res.add(new Pair(i, ita.y));
             }
-            if (itb != null && itb.get().x < S.get(i).y + K) {
-                res.add(new Pair(i, itb.get().y));
+            if (itb != null && itb.x < S.get(i).y + K) {
+                res.add(new Pair(i, itb.y));
             }
         }
 
@@ -48,34 +49,19 @@ public class Main {
         }
         System.out.println(result);
     }
+}
 
-    static class Pair implements Comparable<Pair> {
-        int x, y;
+class Pair implements Comparable<Pair> {
+    int x, y;
 
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
-        @Override
-        public int compareTo(Pair p) {
-            if (this.x == p.x) {
-                return Integer.compare(this.y, p.y);
-            }
-            return Integer.compare(this.x, p.x);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Pair pair = (Pair) o;
-            return x == pair.x && y == pair.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
+    @Override
+    public int compareTo(Pair o) {
+        if (this.x == o.x) return this.y - o.y;
+        return this.x - o.x;
     }
 }
