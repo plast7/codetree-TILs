e34f0585063618494b481c33ef22651d6e30ff18
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 15:35:47 +0900

    Title: 모든 점 방문하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240601/모든 점 방문하기/visit-all-dots.java
+++ 240601/모든 점 방문하기/visit-all-dots.java
@@ -1,37 +1,35 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
     public static final int MAXN = 1005;
 
     public static int N;
     public static int[] cows = new int[MAXN];
     public static int[][] best = new int[MAXN][2], best2 = new int[MAXN][2];
 
-    public static void mini(int[] a, int idx, int b) {
-        if (b < a[idx]) a[idx] = b;
+    public static void mini(int[] a, int b) {
+        if (b < a[0]) a[0] = b;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Arrays.fill(best[0], Integer.MAX_VALUE);
+        for (int[] row : best) Arrays.fill(row, Integer.MAX_VALUE);
 
         N = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) 
             cows[i] = sc.nextInt();
-        }
-
+        
         cows[++N] = 0;
 
         Arrays.sort(cows, 1, N + 1);
 
-        for (int i = 1; i <= N; i++) {
-            if (cows[i] == 0) {
+        for (int i = 1; i <= N; i++) 
+            if (cows[i] == 0)
                 best[i][0] = 0;
-            }
-        }
 
         for (int len = 1; len < N; len++) {
             int ccount = N - len;
@@ -39,23 +37,20 @@ public class Main {
             for (int[] row : best2) Arrays.fill(row, Integer.MAX_VALUE);
 
             for (int i = 1; i + len <= N + 1; i++) {
-                mini(best2[i - 1], 0, best[i][0] + ccount * (cows[i] - cows[i - 1]));
-                mini(best2[i - 1], 0, best[i][1] + ccount * (cows[i + len - 1] - cows[i - 1]));
+                mini(best2[i - 1], best[i][0] + ccount * (cows[i] - cows[i - 1]));
+                mini(best2[i - 1], best[i][1] + ccount * (cows[i + len - 1] - cows[i - 1]));
 
-                mini(best2[i], 1, best[i][0] + ccount * (cows[i + len] - cows[i]));
-                mini(best2[i], 1, best[i][1] + ccount * (cows[i + len] - cows[i + len - 1]));
+                mini(best2[i], best[i][0] + ccount * (cows[i + len] - cows[i]));
+                mini(best2[i], best[i][1] + ccount * (cows[i + len] - cows[i + len - 1]));
             }
 
-            for (int i = 0; i < best.length; i++) {
-                best[i][0] = best2[i][0];
-                best[i][1] = best2[i][1];
-            }
+            for (int i = 0; i < best.length; i++)
+                best[i] = Arrays.copyOf(best2[i], best2[i].length);
         }
 
-        if (best[1][1] < best[1][0]) {
-            best[1][0] = best[1][1];
-        }
-
+        best[1][0] = Math.min(best[1][0], best[1][1]);
         System.out.println(best[1][0]);
+
+        sc.close();
     }
 }
