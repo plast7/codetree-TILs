53e61acff4afca309c1156e9ef3e43528dd8b5cc
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 15:28:14 +0900

    Title: 겹쳐있는 정사각형의 총 면적, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240601/겹쳐있는 정사각형의 총 면적/the-total-area-of-the-overlapping-square.java
+++ 240601/겹쳐있는 정사각형의 총 면적/the-total-area-of-the-overlapping-square.java
@@ -7,38 +7,34 @@ public class Main {
         int N = sc.nextInt();
         int K = sc.nextInt();
 
-        List<Pair> S = new ArrayList<>();
-        for(int i = 0; i < N; i++) {
+        List<Pair<Integer, Integer>> S = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            S.add(new Pair(x, y));
+            S.add(new Pair<>(x, y));
         }
-        Collections.sort(S);
+        S.sort(Comparator.comparing(Pair::getKey));
 
-        TreeSet<PairWithIndex> st = new TreeSet<>();
-        List<Pair> res = new ArrayList<>();
-        Iterator<PairWithIndex> ita, itb;
-        for(int i = 0, j = 0; i < S.size() && res.size() < 2; i++) {
-            for(; S.get(j).x + K <= S.get(i).x; j++) {
-                st.remove(new PairWithIndex(S.get(j).y, j));
+        TreeSet<Pair<Integer, Integer>> st = new TreeSet<>(Comparator.comparing(Pair::getKey).thenComparing(Pair::getValue));
+        List<Pair<Integer, Integer>> res = new ArrayList<>();
+        Iterator<Pair<Integer, Integer>> ita, itb;
+
+        for (int i = 0, j = 0; i < S.size() && res.size() < 2; i++) {
+            for (; S.get(j).getKey() + K <= S.get(i).getKey(); j++) {
+                st.remove(new Pair<>(S.get(j).getValue(), j));
             }
 
-            PairWithIndex inserted = new PairWithIndex(S.get(i).y, i);
-            st.add(inserted);
-            ita = st.headSet(inserted, false).isEmpty() ? null : st.headSet(inserted, false).iterator();
-            itb = st.tailSet(inserted, false).isEmpty() ? null : st.tailSet(inserted, false).iterator();
+            Pair<Integer, Integer> insertedPair = new Pair<>(S.get(i).getValue(), i);
+            ita = st.headSet(insertedPair, true).descendingIterator();
+            itb = st.tailSet(insertedPair, false).iterator();
 
-            if (ita != null && ita.hasNext()) {
-                PairWithIndex itaPair = ita.next();
-                if (S.get(i).y < itaPair.value + K) {
-                    res.add(new Pair(i, itaPair.index));
-                }
+            st.add(insertedPair);
+
+            if (ita.hasNext() && S.get(i).getValue() < ita.next().getKey() + K) {
+                res.add(new Pair<>(i, ita.next().getValue()));
             }
-            if (itb != null && itb.hasNext()) {
-                PairWithIndex itbPair = itb.next();
-                if (itbPair.value < S.get(i).y + K) {
-                    res.add(new Pair(i, itbPair.index));
-                }
+            if (itb.hasNext() && itb.next().getKey() < S.get(i).getValue() + K) {
+                res.add(new Pair<>(i, itb.next().getValue()));
             }
         }
 
@@ -46,44 +42,32 @@ public class Main {
         if (res.size() > 1) {
             result = -1;
         } else if (res.size() == 1) {
-            int dx = Math.abs(S.get(res.get(0).x).x - S.get(res.get(0).y).x);
-            int dy = Math.abs(S.get(res.get(0).x).y - S.get(res.get(0).y).y);
+            int dx = S.get(res.get(0).getKey()).getKey() - S.get(res.get(0).getValue()).getKey();
+            int dy = S.get(res.get(0).getKey()).getValue() - S.get(res.get(0).getValue()).getValue();
+            if (dx < 0) dx = -dx;
+            if (dy < 0) dy = -dy;
             result = 1L * (K - dx) * (K - dy);
         }
+        
         System.out.println(result);
+        sc.close();
+    }
+}
+
+class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
-    static class Pair implements Comparable<Pair> {
-        int x, y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            if (this.x != o.x) {
-                return Integer.compare(this.x, o.x);
-            } else {
-                return Integer.compare(this.y, o.y);
-            }
-        }
+    public K getKey() {
+        return key;
     }
 
-    static class PairWithIndex implements Comparable<PairWithIndex> {
-        int value, index;
-        PairWithIndex(int value, int index) {
-            this.value = value;
-            this.index = index;
-        }
-
-        @Override
-        public int compareTo(PairWithIndex o) {
-            if (this.value != o.value) {
-                return Integer.compare(this.value, o.value);
-            } else {
-                return Integer.compare(this.index, o.index);
-            }
-        }
+    public V getValue() {
+        return value;
     }
 }
