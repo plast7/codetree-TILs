b3393917de3a01108a393ea74c30e5fd02f8f906
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 15:34:47 +0900

    Title: 특정 구역 내의 공, Time: 911ms, Memory: 134MB, Status: Passed - CodeTree

--- 240601/특정 구역 내의 공/balls-within-a-particular-zone.java
+++ 240601/특정 구역 내의 공/balls-within-a-particular-zone.java
@@ -1,32 +1,51 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
-    private static final int inf = 1 << 30;
-    private static final int[] mx = {-1, 0, 1, 0};
-    private static final int[] my = {0, -1, 0, 1};
+    public static final int INF = 1 << 30;
     
-    private static int[] x = new int[3010], y = new int[3010], t = new int[3010];
-    private static char[][] arr = new char[6010][6010];
-    private static int gz, xz, yz;
-    private static Stack<Short> sx = new Stack<>(), sy = new Stack<>();
+    public static int[] x = new int[3010], y = new int[3010];
+    public static int[] t = new int[3010];
 
     public static int compress(int[] a, int z) {
         System.arraycopy(a, 0, t, 0, z);
         Arrays.sort(t, 0, z);
-        int nz = (int) Arrays.stream(t, 0, z).distinct().count();
+        int nz = unique(t, z);
         for (int i = 0; i < z; i++) {
-            a[i] = Arrays.binarySearch(t, 0, nz, a[i]) * 2;
+            a[i] = (Arrays.binarySearch(t, 0, nz, a[i])) * 2;
         }
         return nz * 2;
     }
     
+    public static int unique(int[] array, int n) {
+        if (n == 0) return 0;
+        int j = 0;
+        for (int i = 1; i < n; i++) {
+            if (array[i] != array[j]) {
+                j++;
+                array[j] = array[i];
+            }
+        }
+        return j + 1;
+    }
+
+    public static int xz, yz;
+    public static char[][] arr = new char[6010][6010];
+    
+    public static int gz;
+    public static int[] mx = {-1, 0, 1, 0};
+    public static int[] my = {0, -1, 0, 1};
+
+    public static Stack<Short> sx = new Stack<>(), sy = new Stack<>();
+
     public static void put(int x, int y) {
         if (x < 0 || x >= xz || y < 0 || y >= yz) return;
         if (arr[x][y] == 'X') return;
         gz += (arr[x][y] == 'C') ? 1 : 0;
         arr[x][y] = 'X';
-        sx.push((short)x);
-        sy.push((short)y);
+        sx.push((short) x);
+        sy.push((short) y);
     }
 
     public static void dfs(int startx, int starty) {
@@ -41,68 +60,57 @@ public class Main {
             }
         }
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = sc.nextInt(), m = sc.nextInt();
         int z = 0;
-        
-        // Initialize x and y coordinates
-        x[z] = y[z] = -inf; z++;
-        x[z] = y[z] = inf; z++;
-        
-        // Read the coordinates
+        x[z] = y[z] = -INF; z++;
+        x[z] = y[z] = INF; z++;
         for (int i = 0; i < 2 * n; i++) {
             x[z] = sc.nextInt();
-            y[z] = sc.nextInt(); 
-            z++;
+            y[z] = sc.nextInt(); z++;
         }
         for (int i = 0; i < m; i++) {
             x[z] = sc.nextInt();
-            y[z] = sc.nextInt();
-            z++;
+            y[z] = sc.nextInt(); z++;
         }
-        
-        // Compress the coordinates
         xz = compress(x, z);
         yz = compress(y, z);
-
-        // Initialize the array
-        for (int i = 0; i < 6010; i++) {
+        for (int i = 0; i < arr.length; i++) {
             Arrays.fill(arr[i], '.');
         }
-
-        // Fill the array with 'X' as per the input lines
         for (int i = 0; i < n; i++) {
             int a = i * 2 + 2;
             int b = i * 2 + 3;
             int x1 = x[a], y1 = y[a], x2 = x[b], y2 = y[b];
             if (x1 == x2) {
                 if (y1 > y2) {
-                    int temp = y1; y1 = y2; y2 = temp;
+                    int tmp = y1;
+                    y1 = y2;
+                    y2 = tmp;
                 }
-                for (; y1 <= y2; y1++) {
+                while (y1 <= y2) {
                     arr[x1][y1] = 'X';
+                    y1++;
                 }
             } else {
                 if (x1 > x2) {
-                    int temp = x1; x1 = x2; x2 = temp;
+                    int tmp = x1;
+                    x1 = x2;
+                    x2 = tmp;
                 }
-                for (; x1 <= x2; x1++) {
+                while (x1 <= x2) {
                     arr[x1][y1] = 'X';
+                    x1++;
                 }
             }
         }
-
-        // Mark 'C' in the array
         for (int i = 0; i < m; i++) {
             int a = i + 2 * n + 2;
             arr[x[a]][y[a]] = 'C';
         }
-
-        // Perform DFS to find the largest connected component of 'C'
         int ans = 0;
         for (int i = 0; i < m; i++) {
             int a = i + 2 * n + 2;
@@ -110,8 +118,6 @@ public class Main {
             dfs(x[a], y[a]);
             ans = Math.max(ans, gz);
         }
-
-        // Print the result
         System.out.println(ans);
     }
 }
