cd265658388b799298db6d259ae79639254a0359
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-01 21:43:31 +0900

    Title: 겹쳐있는 정사각형의 총 면적, Time: 5096ms, Memory: 31MB, Status: Time Limit Exceed - CodeTree

--- 240601/겹쳐있는 정사각형의 총 면적/the-total-area-of-the-overlapping-square.py
+++ 240601/겹쳐있는 정사각형의 총 면적/the-total-area-of-the-overlapping-square.py
@@ -3,37 +3,40 @@ input = sys.stdin.read
 
 def main():
     data = input().split()
-    
     N = int(data[0])
     K = int(data[1])
-    S = [(int(data[2 + 2 * i]), int(data[3 + 2 * i])) for i in range(N)]
-    
+    S = []
+
+    index = 2
+    for i in range(N):
+        x = int(data[index])
+        y = int(data[index + 1])
+        S.append((x, y))
+        index += 2
+
     S.sort()
-    
+
     st = set()
     res = []
-    
-    i = j = 0
-    
-    while i < len(S) and len(res) < 2:
-        while j < len(S) and S[j][0] + K <= S[i][0]:
+    j = 0
+    for i in range(len(S)):
+        if len(res) >= 2:
+            break
+        while S[j][0] + K <= S[i][0]:
             st.discard((S[j][1], j))
             j += 1
-        
+
         ita = itb = st.add((S[i][1], i))
-        
-        if ita is not None and ita != min(st):
-            ita = sorted(st).index((S[i][1], i)) - 1
-            if S[i][1] < list(st)[ita][0] + K:
-                res.append((i, list(st)[ita][1]))
-        
-        if itb is not None and itb != max(st):
-            itb = sorted(st).index((S[i][1], i)) + 1
-            if itb < len(st) and list(st)[itb][0] < S[i][1] + K:
-                res.append((i, list(st)[itb][1]))
-        
-        i += 1
-    
+        ita = itb = (S[i][1], i)
+        st_sorted = sorted(st)
+        ita_index = st_sorted.index(ita)
+
+        if ita_index > 0 and S[i][1] < st_sorted[ita_index - 1][0] + K:
+            res.append((i, st_sorted[ita_index - 1][1]))
+
+        if ita_index + 1 < len(st) and st_sorted[ita_index + 1][0] < S[i][1] + K:
+            res.append((i, st_sorted[ita_index + 1][1]))
+
     result = 0
     if len(res) > 1:
         result = -1
@@ -41,7 +44,7 @@ def main():
         dx = abs(S[res[0][0]][0] - S[res[0][1]][0])
         dy = abs(S[res[0][0]][1] - S[res[0][1]][1])
         result = (K - dx) * (K - dy)
-    
+
     print(result)
 
 if __name__ == "__main__":
