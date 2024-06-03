68eef62baecd2ec205ef90935b57151dcff02fe1
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-03 22:04:15 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240603/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240603/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,70 +1,46 @@
-#include <iostream>
-#include <algorithm>
-#include <tuple>
-#include <vector>
+import java.util.Scanner
+import kotlin.math.max
 
-#define MAX_N 500
-#define DIR_NUM 4
+fun main() {
+    val sc = Scanner(System.`in`)
 
-using namespace std;
+    val n = sc.nextInt()
+    val grid = List(n) { List(n) { sc.nextInt() } }
+    val dp = List(n) { MutableList(n) { -1 } }
 
-// 변수 선언
-int n;
-int grid[MAX_N][MAX_N];
-int dp[MAX_N][MAX_N];
-
-bool InRange(int x, int y) {
-    return 0 <= x && x < n && 0 <= y && y < n;
-}
-
-// (x, y)에서 출발하여 조건을 만족하며
-// 도달할 수 있는 칸의 수 중
-// 최대 칸의 수를 구합니다.
-int FindMax(int x, int y) {
-    // 이미 계산해본적이 있다면
-    // 그 값을 바로 반환합니다.
-    if(dp[x][y] != -1)
-        return dp[x][y];
-
-    // 기본값은 자기자신이 됩니다.
-    int best = 1;
-
-    // 정수값이 작은 칸부터 순서대로 보며
-    // 4방향에 대해 최적의 칸 수를 계산합니다.
-    int dx[DIR_NUM] = {1, 0, -1, 0};
-    int dy[DIR_NUM] = {0, -1, 0, 1};
-
-    for(int j = 0; j < DIR_NUM; j++) {
-        int nx = x + dx[j], ny = y + dy[j];
-        if(InRange(nx, ny) && grid[nx][ny] > grid[x][y])
-            best = max(best, FindMax(nx, ny) + 1);
+    var result = 0
+    for(i in 0 until n) {
+        for(j in 0 until n) {
+            result = max(result, findMax(i, j, dp, grid, n))
+        }
     }
 
-    return dp[x][y] = best;
+    println(result)
 }
 
-int main() {
-    // 입력
-    cin >> n;
+fun isAvailable(x: Int, y: Int, n: Int): Boolean {
+    return !(x < 0 || y < 0 || x >= n || y >= n)
+}
 
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            cin >> grid[i][j];
+fun findMax(x: Int, y: Int, dp: List<MutableList<Int>>, grid: List<List<Int>>, n: Int): Int {
+    if(dp[y][x] != -1) {
+        return dp[y][x]
+    }
 
-    // 메모이제이션을 위해 
-    // 전부 초기값을 -1로 셋팅해줍니다.
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            dp[i][j] = -1;
+    var ans = 1
 
-    // 각 칸에 시작했을 떄
-    // 최대로 이동할 수 있는 칸의 수 중 
-    // 최댓값을 갱신합니다.
-    int ans = 0;
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
-            ans = max(ans, FindMax(i, j));
-    
-    cout << ans;
-    return 0;
+    val moveX = listOf(1, 0, -1, 0)
+    val moveY = listOf(0, -1, 0, 1)
+
+    for(k in moveX.indices) {
+        val nextX = x + moveX[k]
+        val nextY = y + moveY[k]
+
+        if (isAvailable(nextX, nextY, n) && grid[nextY][nextX] > grid[y][x]) {
+            ans = max(ans, findMax(nextX, nextY, dp, grid, n) + 1)
+        }
+    }
+
+    dp[y][x] = ans
+    return dp[y][x]
 }
