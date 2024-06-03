a94f824ca55996a7cf470b5e4959d0856c4a94a1
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-03 21:51:51 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240603/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.kt
+++ 240603/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.kt
@@ -1,5 +1,49 @@
-import java.util.Scanner
-import kotlin.math.max
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val n = sc.nextInt()
+    val grid = List(n) { List(n) { sc.nextInt() } }
+    val dp = List(n) { MutableList(n) { -1 } }
+
+    var result = 0
+    for(i in n-1 downTo 0) {
+        for(j in 0 until n) {
+            result = max(result, findMax(i, j, dp, grid, n))
+        }
+    }
+
+    println(result)
+}
+
+fun isAvailable(x: Int, y: Int, n: Int): Boolean {
+    return !(x < 0 || y < 0 || x >= n || y >= n)
+}
+
+fun findMax(x: Int, y: Int, dp: List<MutableList<Int>>, grid: List<List<Int>>, n: Int): Int {
+    if(dp[y][x] != -1) {
+        return dp[y][x]
+    }
+
+    var ans = 1
+
+    val moveX = listOf(1, 0, -1, 0)
+    val moveY = listOf(0, -1, 0, 1)
+
+    for(k in moveX.indices) {
+        val nextX = x + moveX[k]
+        val nextY = y + moveY[k]
+
+        if (isAvailable(nextX, nextY, n) && grid[nextY][nextX] < grid[y][x]) {
+            ans = max(ans, findMax(nextX, nextY, dp, grid, n) + 1)
+        }
+    }
+
+    dp[y][x] = ans
+    return dp[y][x]
+}
+
+
+
 
 fun main() {
     val sc = Scanner(System.`in`)
@@ -22,7 +66,7 @@ fun isAvailable(x: Int, y: Int, n: Int): Boolean {
     return !(x < 0 || y < 0 || x >= n || y >= n)
 }
 
-tailrec fun findMax(x: Int, y: Int, dp: List<MutableList<Int>>, grid: List<List<Int>>, n: Int): Int {
+fun findMax(x: Int, y: Int, dp: List<MutableList<Int>>, grid: List<List<Int>>, n: Int): Int {
     if(dp[y][x] != -1) {
         return dp[y][x]
     }
@@ -36,7 +80,7 @@ tailrec fun findMax(x: Int, y: Int, dp: List<MutableList<Int>>, grid: List<List<
         val nextX = x + moveX[k]
         val nextY = y + moveY[k]
 
-        if (isAvailable(nextX, nextY, n) && grid[nextY][nextX] < grid[y][x]) {
+        if (isAvailable(nextX, nextY, n) && grid[nextY][nextX] > grid[y][x]) {
             ans = max(ans, findMax(nextX, nextY, dp, grid, n) + 1)
         }
     }
