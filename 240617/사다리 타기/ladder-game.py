c986e933bb55e09486a562e3e4ae8a005672febc
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-17 19:21:26 +0900

    Title: 사다리 타기, Time: 197ms, Memory: 26MB, Status: Passed - CodeTree

--- 240617/사다리 타기/ladder-game.py
+++ 240617/사다리 타기/ladder-game.py
@@ -1,56 +1,53 @@
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
+
+lines = list()
+selected_lines = list()
+
+ans = m
+
+# 처음 상황과, 선택한 가로줄만 사용했을 때의
+# 상황을 시뮬레이션하여
+# 둘의 결과가 같은지 확인합니다.
 def possible():
-    
-    # 1: 초기화
-    num1 = [i for i in range(n)]
-    num2 = [i for i in range(n)]
+    # Step1. 시작 숫자를 셋팅합니다.
+    num1, num2 = [i for i in range(n)], [i for i in range(n)]
 	
-    # 2: 반영
+    # Step2. 위에서부터 순서대로 적혀있는 
+    # 가로줄에 대해 양쪽 번호에 해당하는 숫자를 바꿔줍니다. 
     for _, idx in lines:
         num1[idx], num1[idx + 1] = num1[idx + 1], num1[idx]
-    
     for _, idx in selected_lines:
         num2[idx], num2[idx + 1] = num2[idx + 1], num2[idx]
 	
-    # 3: 판단
+    # Step3. 두 상황의 결과가 동일한지 확인합니다.
     for i in range(n):
         if num1[i] != num2[i]:
             return False
 
     return True
 
-def backTraking(cnt, idx):
+
+def find_min_lines(cnt):
+    global ans
     
-    # 기저조건
-    if cnt == k:
+    if cnt == m:
         if possible():
-            global min_res
-            min_res = min(min_res, k)
+            ans = min(ans, len(selected_lines))
         return
     
-    # 백트래킹
-    for i in range(idx, m):
-        selected_lines.append(lines[i])
-        backTraking(cnt + 1, idx + 1)
-        selected_lines.pop()
+    selected_lines.append(lines[cnt])
+    find_min_lines(cnt + 1)
+    selected_lines.pop()
+	
+    find_min_lines(cnt + 1)
 
-# 입력
-import sys
-input = sys.stdin.readline
 
-n, m = map(int, input().split())
-lines = []
 for _ in range(m):
-    a, b = map(int, input().split())
+    a, b = tuple(map(int, input().split()))
     lines.append((b, a - 1))
 
-# 계산1: 정렬(사다리 위)
 lines.sort()
 
-# 계산2: 백트래킹
-min_res = m
-for k in range(m+1):
-    selected_lines = []
-    backTraking(0, 0)
-
-# 출력
-print(min_res)
+find_min_lines(0)
+print(ans)
