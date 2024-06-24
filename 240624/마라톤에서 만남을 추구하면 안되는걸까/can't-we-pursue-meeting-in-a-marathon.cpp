db44d0c18b242b3b634a06592cb4a808acc6d5ef
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-06-24 13:25:02 +0900

    Title: 마라톤에서 만남을 추구하면 안되는걸까, Time: 80ms, Memory: 2MB, Status: Passed - CodeTree

--- 240624/마라톤에서 만남을 추구하면 안되는걸까/can't-we-pursue-meeting-in-a-marathon.cpp
+++ 240624/마라톤에서 만남을 추구하면 안되는걸까/can't-we-pursue-meeting-in-a-marathon.cpp
@@ -4,105 +4,99 @@
 
 using namespace std;
 
-#define MAX_N 100000
-
-// 달리는 사람의 속도와 모듈러 값을 저장하는 구조체입니다.
+// 각 달리기 선수를 나타내는 구조체입니다.
 struct Runner {
-    long long speed;
-    long long modulus;
-    int rank;
+    long long speed;  // 속력
+    long long modulus;  // (L * C * speed) % (C * max_speed) 값
+    int rank;  // modulus 값에 따른 순위
 };
 
-// 변수 선언
 int n, l, c;
-Runner runners[MAX_N];
-int bit[MAX_N + 1];
-int bitlen;
+vector<Runner> runners;
+vector<int> bit;  // Binary Indexed Tree (BIT) 배열입니다.
 
 // BIT 초기화 함수입니다.
-void bit_init(int n) {
-    for(int i = 1; i <= n; i++) {
-        bit[i] = 0;
-    }
-    bitlen = n;
+void bit_init(int size) {
+    bit.assign(size + 1, 0);
 }
 
-// BIT에서 i까지의 prefix sum을 구하는 함수입니다.
-int bit_prefix_sum(int i) {
+// BIT에서 index 위치의 값을 증가시키는 함수입니다.
+void bit_inc(int index) {
+    while (index < bit.size()) {
+        bit[index]++;
+        index += index & -index;
+    }
+}
+
+// BIT에서 index 위치까지의 합을 구하는 함수입니다.
+int bit_prefix_sum(int index) {
     int sum = 0;
-    for(int j = i; j > 0; j -= (j & (-j))) {
-        sum += bit[j];
+    while (index > 0) {
+        sum += bit[index];
+        index -= index & -index;
     }
     return sum;
 }
 
-// BIT에서 i 위치의 값을 증가시키는 함수입니다.
-void bit_inc(int i) {
-    for(int j = i; j <= bitlen; j += (j & (-j))) {
-        bit[j]++;
-    }
-}
-
-// 모듈러 값을 기준으로 정렬하기 위한 비교 함수입니다.
-bool sort_by_modulus(const Runner &a, const Runner &b) {
+// modulus 값을 기준으로 정렬하기 위한 비교 함수입니다.
+bool compare_by_modulus(const Runner &a, const Runner &b) {
     return a.modulus < b.modulus;
 }
 
-// 속도를 기준으로 정렬하기 위한 비교 함수입니다.
-bool sort_by_speed(const Runner &a, const Runner &b) {
+// speed 값을 기준으로 정렬하기 위한 비교 함수입니다.
+bool compare_by_speed(const Runner &a, const Runner &b) {
     return a.speed < b.speed;
 }
 
 int main() {
     // 입력:
     cin >> n >> l >> c;
-
+    runners.resize(n);
     long long max_speed = 0;
-    for(int i = 0; i < n; i++) {
+
+    // 각 선수의 속력을 입력받고, 최대 속력을 구합니다.
+    for (int i = 0; i < n; ++i) {
         cin >> runners[i].speed;
-        if(runners[i].speed > max_speed) {
-            max_speed = runners[i].speed;
-        }
+        max_speed = max(max_speed, runners[i].speed);
     }
 
-    // 각 사람의 속도에 따른 모듈러 값을 계산합니다.
-    for(int i = 0; i < n; i++) {
+    // 각 선수의 modulus 값을 계산합니다.
+    for (int i = 0; i < n; ++i) {
         runners[i].modulus = (l * c * runners[i].speed) % (c * max_speed);
     }
-    // 모듈러 값을 기준으로 정렬합니다.
-    sort(runners, runners + n, sort_by_modulus);
 
-    // 모듈러 값에 따라 랭크를 부여합니다.
+    // modulus 값을 기준으로 정렬합니다.
+    sort(runners.begin(), runners.end(), compare_by_modulus);
+
+    // modulus 값에 따른 순위를 매깁니다.
     int rank = 1;
-    for(int i = 0; i < n; ) {
-        int j = i;
-        while(j < n && runners[i].modulus == runners[j].modulus) {
-            runners[j].rank = rank;
-            j++;
+    for (int i = 0; i < n; ++i) {
+        if (i > 0 && runners[i].modulus != runners[i - 1].modulus) {
+            rank++;
         }
-        i = j;
-        rank++;
+        runners[i].rank = rank;
     }
 
-    // 속도를 기준으로 정렬합니다.
-    sort(runners, runners + n, sort_by_speed);
-    bit_init(n);
+    // 속력 값을 기준으로 정렬합니다.
+    sort(runners.begin(), runners.end(), compare_by_speed);
+
+    // BIT를 초기화합니다.
+    bit_init(rank);
 
     long long total_meetings = 0;
     long long sum_of_floors = 0;
-    for(int i = 0; i < n; i++) {
-        // 현재 사람의 속도에 따른 floor 값을 계산합니다.
+
+    // 각 선수에 대해 만남 횟수를 계산합니다.
+    for (int i = 0; i < n; ++i) {
         long long floor = (l * runners[i].speed) / max_speed;
-        // 현재 사람과 이전 사람들 간의 만남 횟수를 계산합니다.
         long long addition = floor * i - sum_of_floors - i + bit_prefix_sum(runners[i].rank);
-
         total_meetings += addition;
-
         sum_of_floors += floor;
         bit_inc(runners[i].rank);
     }
 
-    // 총 만남 횟수를 출력합니다.
+    // 결과를 출력합니다.
     cout << total_meetings << endl;
+
     return 0;
 }
