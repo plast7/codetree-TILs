e378343c2233efb3b37ab3bf29706021c159c63b
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-09-14 00:47:51 +0900

    Title: 중앙값 계산 2, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240914/중앙값 계산 2/get-median-2.py
+++ 240914/중앙값 계산 2/get-median-2.py
@@ -1,6 +1,6 @@
 n=int(input())
 if n!=0:
-    listNum=list(map(int,input().split(" ")))
+    listNum=list(map(int,input().split()))
     for i in range(1,n+1):
         if i%2!=0:
             listSum=listNum[0:i]
