8acb2e94dd375c8aa54626a2db068b6ece2ec195
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-09-19 19:28:40 +0900

    Title: k에 가장 가까운 합, Time: 189ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240919/k에 가장 가까운 합/sum-closest-to-k.cpp
+++ 240919/k에 가장 가까운 합/sum-closest-to-k.cpp
@@ -1,65 +1,66 @@
 #include <iostream>
 #include <algorithm>
-#include <tuple>
-#include <climits> 
-#include <unordered_map> 
+#include <climits>
 
 using namespace std;
 
-int n, k; 
-#define N_MAX (1'000'000 + 1)
-int arr[N_MAX]; 
-int ans = INT_MAX;
-long long cnt = 0; 
-
 int main() {
-    cin>>n>>k; 
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
-    }
-    sort(arr, arr+n); 
+    // 변수 선언
+    int num_elements, target_sum;
+    // 첫 번째 줄에서 n과 k를 입력받습니다.
+    cin >> num_elements >> target_sum;
     
-    int start=0;
-    int end = n-1;
-    while(start<end){
-        int val = arr[start] + arr[end];
-        val = abs(val-k);
-        // if val > ans -> do not touch 
-        // 
-        if(val<ans){
-            cnt = 1;
-            ans = val; 
+    // n개의 정수를 저장할 배열을 선언합니다.
+    int elements[num_elements];
+    // 두 번째 줄에서 n개의 정수를 입력받습니다.
+    for(int i = 0; i < num_elements; i++) {
+        cin >> elements[i];
+    }
+    
+    // 배열을 오름차순으로 정렬합니다.
+    sort(elements, elements + num_elements);
+    
+    // 투 포인터를 사용하기 위해 시작 포인터와 끝 포인터를 설정합니다.
+    int start = 0;
+    int end = num_elements - 1;
+    // k에 가장 가까운 합의 차이를 저장할 변수를 초기화합니다.
+    int closest_diff = INT_MAX;
+    // k에 가장 가까운 합이 나오는 경우의 수를 저장할 변수를 초기화합니다.
+    long long count = 0;
+    
+    // 투 포인터를 사용하여 배열을 탐색합니다.
+    while(start < end) {
+        // 현재 두 포인터가 가리키는 두 수의 합을 계산합니다.
+        int current_sum = elements[start] + elements[end];
+        // 현재 두 수의 합과 k의 차이를 계산합니다.
+        int current_diff = abs(current_sum - target_sum);
+        
+        // 현재 차이가 가장 작은 차이보다 작다면
+        if(current_diff < closest_diff) {
+            // 가장 작은 차이를 갱신하고
+            closest_diff = current_diff;
+            // 경우의 수를 1로 초기화합니다.
+            count = 1;
+        } 
+        // 현재 차이가 가장 작은 차이와 같다면
+        else if(current_diff == closest_diff) {
+            // 경우의 수를 증가시킵니다.
+            count++;
         }
-        else if(val==ans){
-            cnt++; 
-        }
-        // reached the end point 
-        if(start+1==end)
-            break; 
-        int val1 = arr[start+1] + arr[end];
-        int val2 = arr[start] + arr[end-1]; 
-        val1 = abs(k-val1);
-        val2 = abs(k-val2);
-        if(val1<val2)
+        
+        // 현재 두 수의 합이 k보다 작다면
+        if(current_sum < target_sum) {
+            // 시작 포인터를 오른쪽으로 이동시킵니다.
             start++;
-        else if(val2<val1)
-            end--; 
-        else{ // val1 == val2 
-            if(val1==ans)
-                cnt++;
-            end--;  
+        } 
+        // 현재 두 수의 합이 k보다 크거나 같다면
+        else {
+            // 끝 포인터를 왼쪽으로 이동시킵니다.
+            end--;
         }
     }
     
-    cout<<cnt; 
-
+    // k에 가장 가까운 합이 나오는 경우의 수를 출력합니다.
+    cout << count;
     return 0;
 }
-
-
-// -k/2 for each (?)
-// [s+1, e] vs [s, e-1] -> all same
-// [s+1, e]
-// [s, e]
-
-// a1-k/2, a2-k/2, ..., xxx
