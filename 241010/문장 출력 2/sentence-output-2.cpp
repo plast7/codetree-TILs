e296d1543cdcff229271c2b4948cdb15082a9768
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-10 10:47:52 +0900

    Title: 문장 출력 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/문장 출력 2/sentence-output-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // "I Love CodeTree !"라는 문장을 출력합니다.
+    cout << "I Love CodeTree !";
+    // 프로그램이 정상적으로 종료되었음을 알리기 위해 0을 반환합니다.
+    return 0;
+}
