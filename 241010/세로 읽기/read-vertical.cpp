7ccf61414b08a522a222cde5e12b5dcb639c8046
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-10 15:03:10 +0900

    Title: 세로 읽기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/세로 읽기/read-vertical.cpp
+++ 241010/세로 읽기/read-vertical.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     // 최대 5개의 단어를 저장할 수 있는 배열을 선언합니다.
     // 각 단어는 최대 15개의 문자를 가질 수 있습니다.
-    char words[5][16]; 
+    char words[5][16] = {}; 
 
     // 5개의 단어를 입력받습니다.
     for (int i = 0; i < 5; i++) {
