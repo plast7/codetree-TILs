dd79f476cde7aaa3fc666d0b78539e5ad564a6e2
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-10 10:54:08 +0900

    Title: 변수 선언하기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/변수 선언하기 2/declaring-variables-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 x를 선언하고 365로 초기화합니다.
+    int x = 365;
+    // 변수 y를 선언하고 'Y'로 초기화합니다.
+    char y = 'Y';
+
+    // x 값을 출력합니다.
+    cout << x << endl;
+    // y 값을 출력합니다.
+    cout << y;
+
+    // 프로그램이 정상적으로 종료되었음을 알립니다.
+    return 0;
+}
