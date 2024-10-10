93e5b83864b6a038e5a152b391b81f4f556a09f1
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-10 14:49:58 +0900

    Title: 진법 변환 7, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/진법 변환 7/base-conversion-7.cpp
+++ 241010/진법 변환 7/base-conversion-7.cpp
@@ -1,65 +1,83 @@
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include <iostream>
+#include <cmath>
+#include <string>
 
-int n, m;
+using namespace std;
 
-char s[100];
-int v[200];
-char d[200];
+// 정수부와 소수부를 저장할 변수 선언
+int integerPart, fractionalPart;
+// 소수부를 문자열로 저장할 배열
+char fractionalString[100];
+// 각 자리의 값을 저장할 배열
+int digitValue[200];
+// 각 자리의 문자를 저장할 배열
+char digitChar[200];
 
-void print(int num, int base) {
-  if (num == 0) {
-    printf("0");
-    return;
-  }
+// 정수를 이진법으로 변환하여 출력하는 함수
+void printIntegerInBinary(int number, int base) {
+    // number가 0일 경우 바로 0을 출력하고 종료합니다.
+    if (number == 0) {
+        cout << "0";
+        return;
+    }
 
-  int c = 0;
-  int r[101];
+    // 나머지를 저장할 배열과 인덱스 변수 선언
+    int index = 0;
+    int remainder[101];
 
-  while (num) {
-    r[c++] = d[num % base];
-    num /= base;
-  }
+    // number가 0이 될 때까지 나누기를 반복합니다.
+    while (number) {
+        // 나머지를 저장합니다.
+        remainder[index++] = digitChar[number % base];
+        // number를 base로 나눕니다.
+        number /= base;
+    }
 
-  for (int i = c - 1; i >= 0; i--) {
-    printf("%c", r[i]);
-  }
+    // 나머지를 역순으로 출력하여 이진법으로 변환된 값을 출력합니다.
+    for (int i = index - 1; i >= 0; i--) {
+        cout << remainder[i];
+    }
 }
 
 int main() {
-  for (int i = '0'; i <= '9'; i++) {
-    v[i] = i - '0';
-    d[i - '0'] = i;
-  }
-  for (int i = 'a'; i <= 'z'; i++) {
-    v[i] = i - 'a' + 10;
-    d[i - 'a' + 10] = i;
-  }
-
-  int base = 10;
-  int target = 2;
-
-  scanf("%d.%s", &n, s);
-
-  for (int i = 0; i < 4; i++) {
-    if ('0' <= s[i] && s[i] <= '9') {
-      m = m * 10 + s[i] - '0';
+    // 숫자와 문자를 매핑하는 배열을 초기화합니다.
+    for (int i = '0'; i <= '9'; i++) {
+        digitValue[i] = i - '0';
+        digitChar[i - '0'] = i;
     }
-    else {
-      m *= 10;
+    for (int i = 'a'; i <= 'z'; i++) {
+        digitValue[i] = i - 'a' + 10;
+        digitChar[i - 'a' + 10] = i;
     }
-  }
 
-  print(n, target);
+    // 10진법과 2진법을 나타내는 변수
+    int base = 10;
+    int targetBase = 2;
 
-  printf(".");
+    // 입력을 받아 정수부와 소수부를 분리합니다.
+    scanf("%d.%s", &integerPart, fractionalString);
 
-  for (int i = 0; i < 4; i++) {
-    m *= 2;
-    printf("%d", m / 10000);
-    m %= 10000;
-  }
+    // 소수부를 정수로 변환합니다.
+    for (int i = 0; i < 4; i++) {
+        if ('0' <= fractionalString[i] && fractionalString[i] <= '9') {
+            fractionalPart = fractionalPart * 10 + fractionalString[i] - '0';
+        } else {
+            fractionalPart *= 10;
+        }
+    }
 
-  return 0;
+    // 정수부를 이진법으로 변환하여 출력합니다.
+    printIntegerInBinary(integerPart, targetBase);
+
+    // 소수부를 이진법으로 변환하여 출력합니다.
+    cout << ".";
+
+    // 소수부를 4자리까지 이진법으로 변환합니다.
+    for (int i = 0; i < 4; i++) {
+        fractionalPart *= 2;
+        cout << fractionalPart / 10000;
+        fractionalPart %= 10000;
+    }
+
+    return 0;
 }
