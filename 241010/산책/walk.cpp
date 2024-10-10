39c5c81035ff8b3dfe3f33baaa586e0b32176651
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-10 15:09:24 +0900

    Title: 산책, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/산책/walk.cpp
+++ 241010/산책/walk.cpp
@@ -1,42 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
 #include <iostream>
 #include <map>
-#include <string>
-
+#include <vector>
+#include <algorithm>
+#include <set>
 using namespace std;
 
+int n;
+map<string, int> m;
+string s1, s2;
+
 int main() {
-    // 복장의 개수를 저장할 변수입니다.
-    int numberOfClothes;
-    cin >> numberOfClothes;
+  scanf("%d", &n);
 
-    // 각 복장 종류별로 몇 개의 복장이 있는지를 저장할 map입니다.
-    map<string, int> clothingTypeCount;
-    string clothingName, clothingType;
+  for (int i = 0; i < n; i++) {
+    cin >> s1 >> s2;
 
-    // 모든 복장에 대해 입력을 받습니다.
-    for (int i = 0; i < numberOfClothes; i++) {
-        cin >> clothingName >> clothingType;
-
-        // 해당 복장 종류가 처음 등장하는 경우, map에 추가합니다.
-        if (clothingTypeCount.find(clothingType) == clothingTypeCount.end()) {
-            clothingTypeCount[clothingType] = 1;
-        } else {
-            // 이미 존재하는 복장 종류라면, 개수를 증가시킵니다.
-            clothingTypeCount[clothingType]++;
-        }
+    if (m.find(s2) == m.end()) {
+      m.insert(make_pair(s2, 1));
     }
-
-    // 가능한 코디의 총 수를 계산할 변수입니다.
-    int totalCombinations = 1;
-
-    // 각 복장 종류별로 코디의 경우의 수를 계산합니다.
-    // 각 종류별로 해당 종류의 복장을 착용하지 않는 경우도 포함하여 +1을 해줍니다.
-    for (auto it = clothingTypeCount.begin(); it != clothingTypeCount.end(); ++it) {
-        totalCombinations *= (it->second + 1);
+    else {
+      m[s2]++;
     }
+  }
 
-    // 전체 경우의 수에서 아무것도 착용하지 않는 경우를 제외하기 위해 -1을 해줍니다.
-    cout << totalCombinations - 1 << endl;
+  int s = 1;
 
-    return 0;
+  for (std::map<string, int>::iterator iter = m.begin(); iter != m.end(); ++iter) {
+    s *= (iter->second + 1);
+  }
+
+  printf("%d\n", s - 1);
+
+  return 0;
 }
