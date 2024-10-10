27ff3c8119954bd2bda0139cdddcc00b89c3c069
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-10 14:59:06 +0900

    Title: 세로 읽기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/세로 읽기/read-vertical.cpp
+++ 241010/세로 읽기/read-vertical.cpp
@@ -4,21 +4,20 @@
 using namespace std;
 
 int main() {
-    // 최대 5개의 단어를 저장할 배열을 선언합니다. 각 단어의 최대 길이는 15입니다.
-    char words[5][16]; 
+    char words[5][16]; // 최대 5개의 단어를 저장할 배열을 선언합니다. 각 단어는 최대 15개의 문자로 구성됩니다.
 
     // 5개의 단어를 입력받습니다.
     for (int i = 0; i < 5; i++) {
-        cin >> words[i];
+        cin >> words[i]; // 각 단어를 배열에 저장합니다.
     }
 
-    // 각 단어의 최대 길이인 15까지 열을 순회합니다.
-    for (int col = 0; col < 15; col++) {
-        // 각 단어를 순회합니다.
+    // 최대 15개의 열을 순회합니다.
+    for (int column = 0; column < 15; column++) {
+        // 5개의 행(단어)을 순회합니다.
         for (int row = 0; row < 5; row++) {
-            // 현재 문자가 널 종단 문자가 아닌 경우에만 출력합니다.
-            if (words[row][col] != '\0') {
-                cout << words[row][col];
+            // 현재 문자가 null이 아닌지 확인합니다. (문자열의 끝이 아님을 의미)
+            if (words[row][column] != '\0') {
+                cout << words[row][column]; // null이 아니라면 해당 문자를 출력합니다.
             }
         }
     }
