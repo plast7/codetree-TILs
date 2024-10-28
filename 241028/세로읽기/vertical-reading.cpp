f19b2d37048c8ee990fea8892072035b9163e04e
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-28 11:50:36 +0900

    Title: 세로읽기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241028/세로읽기/vertical-reading.cpp
+++ 241028/세로읽기/vertical-reading.cpp
@@ -1,19 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-string a[10];
+// 각 줄의 문자들을 저장하는 배열입니다.
+char a[5][16];
 
-int main() {
-    for(int i = 0; i < 4; i++) cin >> a[i];
-    for(int i = 0; i < 4; i++) {
-        assert(1 <= a[i].length() and a[i].length() <= 15);
-        for(int j = 0; j < a[i].length(); j++) {
-            assert('0' <= a[i][j] and a[i][j] <= '9' or 'a' <= a[i][j] and a[i][j] <= 'z' or 'A' <= a[i][j] and a[i][j] <= 'Z');
-        }
-    }
-    for(int j = 0; j < 15; j++) {
-        for(int i = 0; i < 4; i++) {
-            if(j < a[i].length()) cout << a[i][j];
+int main(){
+    // 4개의 문자열을 입력받습니다.
+    for(int i = 0; i < 4; i++)
+        cin >> a[i];
+    
+    // 각 열에 대해 수행합니다.
+    for(int j = 0; j < 15; j++)
+    {
+        // 각 행의 문자를 순서대로 출력합니다.
+        for(int i = 0; i < 4; i++)
+        {
+            // 해당 위치에 문자가 있다면 출력합니다.
+            if(a[i][j] != '\0')
+                cout << a[i][j];
         }
     }
 
