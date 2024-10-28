fdc00ef84468b4f460cd253e8db1778ea519400e
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-10-28 11:49:47 +0900

    Title: 세로읽기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241028/세로읽기/vertical-reading.cpp
+++ 241028/세로읽기/vertical-reading.cpp
@@ -5,11 +5,17 @@ string a[10];
 
 int main() {
     for(int i = 0; i < 4; i++) cin >> a[i];
+    for(int i = 0; i < 4; i++) {
+        assert(1 <= a[i].length() and a[i].length() <= 15);
+        for(int j = 0; j < a[i].length(); j++) {
+            assert('0' <= a[i][j] and a[i][j] <= '9' or 'a' <= a[i][j] and a[i][j] <= 'z' or 'A' <= a[i][j] and a[i][j] <= 'Z');
+        }
+    }
     for(int j = 0; j < 15; j++) {
         for(int i = 0; i < 4; i++) {
             if(j < a[i].length()) cout << a[i][j];
         }
-    }    
+    }
 
     return 0;
 }
