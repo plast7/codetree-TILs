ef30ef5bcae115217f45abddbd807229481744e9
Jin Kim <92560356+plast7@users.noreply.github.com>
2024-12-19 13:52:31 +0900

    Title: 알바로 부자 되기, Time: 81ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241219/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+board = [[x for x in map(int, input().split())] for _ in range(n)]
+
+dp = [x[2] for x in board]
+
+
+board.sort(key=lambda x: x[1])
+
+
+answer = 0
+
+for i in range(1, n):
+    for j in range(i):
+        if board[j][1] < board[i][0]:
+            dp[i] = max(dp[i], dp[j] + board[i][2])
+
+print(max(dp))
